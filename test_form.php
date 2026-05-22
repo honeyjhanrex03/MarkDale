8c6fb9f86404d49f1a42f754bea54e9d166ada71
honeyjhanrex03 <honeyjhanrex03@users.noreply.github.com>
2026-05-22 22:30:26 +0800

    Clean up test files and archives

--- test_form.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-$_SERVER['REQUEST_METHOD'] = 'POST';
-$_POST['name'] = 'Test';
-$_POST['email'] = 'test@example.com';
-$_POST['subject'] = 'Test Subject';
-$_POST['message'] = 'This is a test message.';
-require 'contact.php';
-var_dump($_SESSION);
